d2072bbf4eaff605663ea1000e43ca43404e30f0
abdulaziz-mansour <abdulaziz.mansour.work@gmail.com>
2026-01-21 19:01:17 +0300

    solv the task

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,22 @@ import connectDB from "./database";
 import authorRouter from "./api/author/authors.routes";
 import postRouter from "./api/posts/posts.routes";
 import tagRouter from "./api/tag/tag.routes";
-
+import morgan from "morgan";
+import { notFound } from "./middlewares/notFound.middleware";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/posts", postRouter);
 app.use("/authors", authorRouter);
 app.use("/tags", tagRouter);
 
+app.use(notFound);
+app.use(errorHandler);
 
 connectDB();
 app.listen(PORT, () => {
